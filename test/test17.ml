0166dc39f11c9d18882a5fbb790aa03f58dfde50
Makhor01 <60820727+Makhor01@users.noreply.github.com>
2024-11-19 18:31:39 +0300

    Update test17.ml

--- test/test17.ml
+++ test/test17.ml
@@ -5,6 +5,7 @@ let task17_implementations =
     ("Module implementation", Task17.Module.solve);
     ("Map based implementation", Task17.Map.solve);
     ("For loop based implementation", Task17.For.solve);
+    ("Infinite sequences based algorithm", Task17.Inf.solve);
   ]
 
 let task17_checker solve_fun () =
